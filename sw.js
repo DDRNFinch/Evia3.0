3bd463fbea08068c2948c3ac99c0056612da47c7
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-09-14 01:20:43 +0100

    Force Evia activity UI cache refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'evia3-v5';
+const CACHE = 'evia3-v6';
 const ASSETS = ['./','./index.html','./styles.css','./activity-tidy.css','./avatar.js','./course.js','./app.js','./advice.js','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
