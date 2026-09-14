4d7d266d71e7077a1930ec2dcb00d4184613cbf9
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-09-14 01:10:17 +0100

    Fix Evia cache so latest UI and advice load

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'evia3-v2';
+const CACHE = 'evia3-v3';
 const ASSETS = ['./','./index.html','./styles.css','./avatar.js','./course.js','./app.js','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
